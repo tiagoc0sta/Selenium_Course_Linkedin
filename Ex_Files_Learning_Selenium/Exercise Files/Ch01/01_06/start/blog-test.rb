fe57934a90965c1536386b95c371931c0cbf60f4
Tiago Costa <tiagodpv@gmail.com>
2023-06-29 16:33:16 -0400

    test running

--- Ex_Files_Learning_Selenium/Exercise Files/Ch01/01_06/start/blog-test.rb
+++ Ex_Files_Learning_Selenium/Exercise Files/Ch01/01_06/start/blog-test.rb
@@ -5,15 +5,16 @@ require "rspec"
 describe "Blog application" do
   describe "signup to the blog application" do
     it "confirm that a user can successfully signup" do
+			timestamp = Time.now.to_i
       driver = Selenium::WebDriver.for :firefox
   		# Go to signup form
   		driver.navigate.to "https://selenium-blog.herokuapp.com/signup"
   		# Fill out and submit form
   		username_field = driver.find_element(id: 'user_username')
-  		username_field.send_keys("user")
+  		username_field.send_keys("user #{timestamp}")
 
   		email_field = driver.find_element(id: 'user_email')
-  		email_field.send_keys("user@test.com")
+  		email_field.send_keys("user#{timestamp}@test.com")
 
   		password_field = driver.find_element(id: 'user_password')
   		password_field.send_keys("password")
@@ -24,7 +25,7 @@ describe "Blog application" do
   		# Confirm user is signed up successfully
   		banner = driver.find_element(id: 'flash_success')
   		banner_text = banner.text
-  		expect(banner_text).to eq("Welcome to the alpha blog user")
+  		expect(banner_text).to eq("Welcome to the alpha blog user #{timestamp}")
 
   		driver.quit
 	  end
